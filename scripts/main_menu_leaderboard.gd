3746cf8b8bcf44bf7cc707c795049396ee12f2d7
serious-hats-only <120053464+serious-hats-only@users.noreply.github.com>
2025-06-28 10:38:09 -0700

    main menu gnome shader

--- scripts/main_menu_leaderboard.gd
+++ scripts/main_menu_leaderboard.gd
@@ -1,18 +1,18 @@
-extends Panel
+extends Sprite2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	float_leaderboard_up()
+	float_gnome_up()
 	
-func float_leaderboard_up():
+func float_gnome_up():
 	var tween = create_tween()
 	tween.tween_property(self, "position", position + Vector2(0, 45), 1.2).set_trans(Tween.TRANS_QUAD)
 	#tween.parallel().tween_property(self, "rotation", rotation + -0.15, 1.0).set_trans(Tween.TRANS_QUAD)
-	tween.tween_callback(float_leaderboard_down)
+	tween.tween_callback(float_gnome_down)
 
-func float_leaderboard_down():
+func float_gnome_down():
 	var tween = create_tween()
 	tween.tween_property(self, "position", position + Vector2(0,-45), 1.2).set_trans(Tween.TRANS_QUAD)
 	#tween.parallel().tween_property(self, "rotation", rotation + 0.15, 1.0).set_trans(Tween.TRANS_QUAD)
-	tween.tween_callback(float_leaderboard_up)
+	tween.tween_callback(float_gnome_up)
