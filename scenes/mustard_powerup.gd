ece36b1a4bac7bab442c31f1df630cc39268a71c
mgillis11235 <119231778+mgillis11235@users.noreply.github.com>
2025-07-03 16:56:18 -0500

    Continuing mustard

--- scenes/mustard_powerup.gd
+++ scenes/mustard_powerup.gd
@@ -1,12 +1,12 @@
 extends Area2D
 
-@export var powerup_type: String = "hotdog"
-@export var duration: float = 15.0
+@export var powerup_type: String = "mustard"
+@export var duration: float = 5.0
 
 func _ready():
 	connect("body_entered", _on_body_entered)
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		body.apply_powerup("hotdog", 15.0)
+		body.apply_powerup("mustard", 5.0)
 		queue_free()
