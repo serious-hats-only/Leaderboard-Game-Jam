d73ff1e45dd95ded3cbb1bf344b88162943d0fa3
mgillis11235 <119231778+mgillis11235@users.noreply.github.com>
2025-07-03 15:14:53 -0500

    Hot dog lengthen

--- scenes/powerup_base.gd
+++ scenes/powerup_base.gd
@@ -1,12 +1,12 @@
 extends Area2D
 
 @export var powerup_type: String = "hotdog"
-@export var duration: float = 10.0
+@export var duration: float = 15.0
 
 func _ready():
 	connect("body_entered", _on_body_entered)
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
-		body.apply_powerup("hotdog", 10.0)
+		body.apply_powerup("hotdog", 15.0)
 		queue_free()
